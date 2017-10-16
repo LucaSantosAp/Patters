69559d1001f9fa59d1727267bcb6dd58bac55b3d
LucaSantosAp <32528200+LucaSantosAp@users.noreply.github.com>
2017-10-16 10:57:51 -0200

    Update Ingrediente.java

--- src/Decorator/Ingrediente.java
+++ src/Decorator/Ingrediente.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Decorator;
 
-/**
- *
- * @author Luciano
- */
+
 public abstract class Ingrediente extends Sobremesa {
     protected Sobremesa sobremesa;
     protected String descricao;
