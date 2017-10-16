302909c9c06321d67caaf4915a6409ed0034b2a4
LucaSantosAp <32528200+LucaSantosAp@users.noreply.github.com>
2017-10-16 10:56:31 -0200

    Update Simples.java

--- src/Composite/Simples.java
+++ src/Composite/Simples.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Composite;
 
-/**
- *
- * @author Luciano
- */
+
 public class Simples implements Produto{
     private String nome;
     private Double preco;
