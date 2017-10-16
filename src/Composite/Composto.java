f29bed64f43eef5e4a20c55f58231cf260653615
LucaSantosAp <32528200+LucaSantosAp@users.noreply.github.com>
2017-10-16 10:55:43 -0200

    Update Composto.java

--- src/Composite/Composto.java
+++ src/Composite/Composto.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Composite;
 
-/**
- *
- * @author Luciano
- */
+
 public class Composto implements Produto{
     private Produto produto1;
     private Produto produto2;
